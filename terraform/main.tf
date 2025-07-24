8a9033b24315c511fac071fae9757e39b2d79f3a
Wendell Segundo <xiamdev1@gmail.com>
2025-07-24 21:48:14 +0800

    apply fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ resource "random_id" "suffix" {
     byte_length = 4
 }
 
-resource "aws_cloudwatchl_log_group" "flask_logs" {
+resource "aws_cloudwatch_log_group" "flask_logs" {
   name = "/aws/ec2/flaskapp"
   retention_in_days = 7
 }
