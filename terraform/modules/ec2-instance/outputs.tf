0736cbb14421238b360eb28706328fe6255bcc47
Wendell Segundo <xiamdev1@gmail.com>
2025-08-14 17:06:03 +0800

    Fix: Path

--- terraform/modules/ec2-instance/outputs.tf
+++ terraform/modules/ec2-instance/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-  value = aws_instance.monitoring_ec2.id
+  value = aws_instance.web-server.id
 }
 
 output "public_ip" {
-  value       = aws_instance.monitoring_ec2.public_ip
+  value       = aws_instance.web-server.id
   description = "Public IP of the monitoring EC2 instance"
 }
 
