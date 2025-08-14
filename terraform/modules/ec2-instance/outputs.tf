acd28854c17d0750c935d1026d7a56db1c29cc34
Wendell Segundo <xiamdev1@gmail.com>
2025-08-14 16:27:26 +0800

    Fix outputs.tf

--- terraform/modules/ec2-instance/outputs.tf
+++ terraform/modules/ec2-instance/outputs.tf
@@ -1,9 +1,10 @@
 output "instance_id" {
-  value = aws_instance.this.id
+  value = aws_instance.monitoring_ec2.id
 }
 
 output "public_ip" {
-  value = aws_instance.this.public_ip
+  value       = aws_instance.monitoring_ec2.public_ip
+  description = "Public IP of the monitoring EC2 instance"
 }
 
 
