5baea05daa30af3d635745d92a7b6848df76fbbb
Wendell Segundo <xiamdev1@gmail.com>
2025-08-14 11:29:51 +0800

    Fix: Docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,7 +83,7 @@ resource "aws_security_group" "monitoring_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
